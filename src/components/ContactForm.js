68d7f4489d5964067724b355887501b468b88db5
Samuel McKean <samuel.mckean@gmail.com>
2020-12-22 20:02:03 -0600

    corrected for to htmlFor

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -5,15 +5,15 @@ function ContactForm(props) {
   return (
     <form name="contact-form">
       <div>
-        <label for="name">Name</label>
+        <label htmlFor="name">Name</label>
         <input type="text" name="name" value={props.name}></input>
       </div>
       <div>
-        <label for="email">Email</label>
+        <label htmlFor="email">Email</label>
         <input type="email" name="email" value={props.email}></input>
       </div>
       <div>
-        <label for="phone">Phone</label>
+        <label htmlFor="phone">Phone</label>
         <input type="tel" name="phone" value={props.phone}></input>
       </div>
       <input type="submit" value="Submit"></input>
