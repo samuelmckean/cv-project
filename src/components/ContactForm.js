8cc14b0a023e25cdfbc06b1654400328d2e8dd52
Samuel McKean <samuel.mckean@gmail.com>
2020-12-22 19:40:40 -0600

    added filled values from props

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -6,15 +6,15 @@ function ContactForm(props) {
     <form name="contact-form">
       <div>
         <label for="name">Name</label>
-        <input type="text" name="name"></input>
+        <input type="text" name="name" value={props.name}></input>
       </div>
       <div>
         <label for="email">Email</label>
-        <input type="email" name="email"></input>
+        <input type="email" name="email" value={props.email}></input>
       </div>
       <div>
         <label for="phone">Phone</label>
-        <input type="tel" name="phone"></input>
+        <input type="tel" name="phone" value={props.phone}></input>
       </div>
       <input type="submit" value="Submit"></input>
     </form>
