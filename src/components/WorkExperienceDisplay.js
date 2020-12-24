a2d1fe1b74f4c5e81d4adbed0e8111b49bda39c1
Samuel McKean <samuel.mckean@gmail.com>
2020-12-24 11:39:52 -0600

    got basic display list going

---
+++ src/components/WorkExperienceDisplay.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function WorkExperienceDisplay(props) {
+  const jobs = props.jobs.map((job) => 
+    <li>
+      <div className="job-container">
+        <div className="company">
+          <h3>Company</h3>
+          <div>{job.company}</div>
+        </div>
+        <div className="title">
+          <h3>Title</h3>
+          <div>{job.title}</div>
+        </div><div className="start-date">
+          <h3>Start Date</h3>
+          <div>{job.startDate}</div>
+        </div><div className="end-date">
+          <h3>End Date</h3>
+          <div>{job.endDate}</div>
+        </div>
+      </div>
+    </li>
+  )
+
+  return (
+    <ul>{jobs}</ul>
+  )
+}
+
+export default WorkExperienceDisplay;
