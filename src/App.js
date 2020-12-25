ffd0669c02cac1732747f786038cc43b567d8325
Samuel McKean <samuel.mckean@gmail.com>
2020-12-24 18:59:13 -0600

    added css import

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import GeneralInfo from './components/GeneralInfo';
 import Education from './components/Education';
 import WorkExperience from './components/WorkExperience';
 
+import './App.css';
+
 function App() {
   return (
     <div className="App">
