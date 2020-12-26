8aad1627c38ac26e34952d9a99f88380cbe73873
Samuel McKean <samuel.mckean@gmail.com>
2020-12-26 09:08:57 -0600

    made WorkExperienceForm match other form components

--- src/components/WorkExperienceForm.js
+++ src/components/WorkExperienceForm.js
@@ -4,13 +4,23 @@ import FormInput from './FormInput';
 function WorkExperienceForm(props) {
 
   return (
-    <form name="work-experience-form" onSubmit={props.handleSubmit}>
-      <FormInput fieldName="company" value={''} />
-      <FormInput fieldName="title" value={''} />
-      <FormInput type="date" fieldName="startDate" value={new Date()} />
-      <FormInput type="date" fieldName="endDate" value={new Date()} />
-      <input type="submit" value="Submit"></input>
-    </form>
+    <div className="row">
+      <form name="work-experience-form" onSubmit={props.handleSubmit}>
+        <div className="col-md-4">
+          <FormInput fieldName="company" value={''} />
+        </div>
+        <div className="col-md-4">
+          <FormInput fieldName="title" value={''} />
+        </div>
+        <div className="col-md-4">
+          <FormInput type="date" fieldName="startDate" value={new Date()} />
+        </div>
+        <div className="col-md-4">
+          <FormInput type="date" fieldName="endDate" value={new Date()} />
+        </div>
+        <input type="submit" value="Submit" className="btn btn-primary"></input>
+      </form>
+    </div>
   )
 }
 
