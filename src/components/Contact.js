9be61359f25ce0a5e62a46ef743a328d98f2456d
Samuel McKean <samuel.mckean@gmail.com>
2020-12-25 19:13:24 -0600

    changed edit button to be on same row as field headers

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,13 +5,13 @@ function Contact(props) {
   return (
     <div className="row">
       <div className="row">
-        <h3 className="col-md-5">Email</h3>
-        <h3 className="col-md-6">Phone</h3>
+        <h3 className="col-md-4">Email</h3>
+        <h3 className="col-md-4">Phone</h3>
+        <button className="col-md-1 offset-md-3" onClick={props.clickEdit}>Edit</button>
       </div>
       <div className="row">
-        <p className="email col-md-5">{props.email}</p>
-        <p className="phone col-md-6">{props.phone}</p>
-        <button className="edit col-md-1" onClick={props.clickEdit}>Edit</button>
+        <p className="email col-md-4">{props.email}</p>
+        <p className="phone col-md-4">{props.phone}</p>
       </div>
     </div>
   )
