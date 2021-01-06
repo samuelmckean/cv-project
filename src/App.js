45e88d6ad1800a54a59f284239923d1e184ee958
Samuel McKean <samuel.mckean@gmail.com>
2021-01-05 21:02:04 -0600

    added theme change buttons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
+import React from 'react';
 import GeneralInfo from './components/GeneralInfo';
 import Education from './components/Education';
 import WorkExperience from './components/WorkExperience';
 
 import './App.css';
+import './styles/dark.css';
+import ThemeChange from './components/ThemeChange';
 
-function App() {
-  return (
-    <div className="App container">
-      <div className="row">
-        <GeneralInfo />
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      theme: 'normal',
+    };
+
+    this.changeTheme = this.changeTheme.bind(this);
+  }
+
+  changeTheme(theme) {
+    this.setState(() => {
+      return {
+        theme,
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="App container">
+        <div className="row">
+          <GeneralInfo />
+        </div>
+        <div className="row">
+          <Education />
+        </div>
+        <div className="row">
+          <WorkExperience />
+        </div>
+        <div className="row">
+          <ThemeChange changeTheme={this.changeTheme} />
+        </div>
       </div>
-      <div className="row">
-        <Education />
-      </div>
-      <div className="row">
-        <WorkExperience />
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
