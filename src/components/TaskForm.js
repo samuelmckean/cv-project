9f5eca39f876126a9b5abd615f13f1cabdffcdac
Samuel McKean <samuel.mckean@gmail.com>
2021-01-11 20:49:14 -0600

    fixed indentation of div element

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -7,14 +7,14 @@ function TaskForm(props) {
   return (
     <div>
       <Tasks tasks={props.tasks} />
-        <div className="row">
-          <div className="col-md-6">
-            <FormInput fieldName="task" value='' />
-          </div>
-          <div className="col-md-2 d-flex align-items-end">
-            <button className="btn btn-secondary" onClick={props.addTask}>Add Task</button>
-          </div>
+      <div className="row">
+        <div className="col-md-6">
+          <FormInput fieldName="task" value='' />
         </div>
+        <div className="col-md-2 d-flex align-items-end">
+          <button className="btn btn-secondary" onClick={props.addTask}>Add Task</button>
+        </div>
+      </div>
     </div>
   )
 }
