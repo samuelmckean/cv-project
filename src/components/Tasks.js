8adcaee692fc6ed0bb1a6ffe99c29f3531a41e1b
Samuel McKean <samuel.mckean@gmail.com>
2021-01-11 21:03:24 -0600

    WIP: added a trash icon from font awesome in Tasks.js

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
 function Tasks(props) {
   const tasks = props.tasks.map((item) => {
-      return <li className="task" key={item.key}>{item.value}</li>
+      return (
+        <li className="task" key={item.key}>
+          <div className="row">
+            {item.value}
+            <button className="btn">
+              <FontAwesomeIcon icon={faTrashAlt} />
+            </button>
+          </div>
+        </li>
+      )
     });
 
   return <ul>{tasks}</ul>
